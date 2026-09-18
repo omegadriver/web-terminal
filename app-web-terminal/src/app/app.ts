0cd50901f2a3da1903c99098067ab46f8a056729
omegadriver <ceo@omegadriver.cloud>
2026-09-18 18:32:39 +0000

    :feature: Adicionado implementacao basica do xterm em componente do Angular

--- app-web-terminal/src/app/app.ts
+++ app-web-terminal/src/app/app.ts
@@ -1,7 +1,31 @@
-import { Component } from '@angular/core';
+// Componente que implementa o xterm.js
+
+import { Component, ElementRef, viewChild, afterNextRender } from '@angular/core';
+import { Terminal } from '@xterm/xterm';
 
 @Component({
-  selector: 'app-root'
+  selector: 'app-root',
+  template: `
+    <div #terminalcontainer></div>
+  `
 })
 export class App {
+  private terminalcontainer = viewChild.required<ElementRef<HTMLDivElement>>('terminalcontainer');
+
+  constructor() {
+    afterNextRender(() => {
+      const terminal = new Terminal();
+      terminal.open(this.terminalcontainer().nativeElement);
+
+      const ws = new WebSocket('ws://localhost:8080');
+
+      ws.onmessage = (event) => {
+        terminal.write(event.toString());
+      };
+
+      terminal.onData((data) => {
+        ws.send(data);
+      });
+    });
+  }
 }
